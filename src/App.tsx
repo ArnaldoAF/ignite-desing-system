a7ff9b3e1630bccba2f58bd76cb87123c3057b62
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-03-22 19:17:21 -0300

    import figma tokens

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export function App() {
   return (
     <>
       <h1 className="font-bold text-5xl">Hello World</h1>
+      <button className="bg-cyan-500 hover:bg-cyan-300 font-medium px-4 py-2 rounded text-white">Button</button>
     </>
   ); 
 }
