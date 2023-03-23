1bb13df80cc9f934f87974e6d38e1edea37176c4
Arnaldo Assis Ferreira <arnaldo_assisferreira@outlook.com>
2023-03-23 18:09:18 -0300

    storybook workflow

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -14,5 +14,12 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
+  },
+  viteFinal: (config, { configuration }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-desing-system/'
+    }
+
+    return config;
   }
 }
